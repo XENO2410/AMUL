ed3a74f2aa1eff87938933feaae2596f3f9d2845
Tarun <97669140+XENO2410@users.noreply.github.com>
2024-05-13 04:16:46 +0530

    Backend push 1

---
+++ backend/config/db.js
@@ -0,0 +1,5 @@
+import mongoose, { connect } from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://amulnsut:b5bqmTeWPxAAKDcN@cluster0.smu6tfj.mongodb.net/amul').then(()=>console.log("DB connected"));
+}
