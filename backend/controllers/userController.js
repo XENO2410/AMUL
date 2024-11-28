66fb07cef0ce48e4a095d77356f4bc9a45b1fe18
Tarun <97669140+XENO2410@users.noreply.github.com>
2024-11-28 14:51:31 +0530

    Backend Host #7

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 // backend\controllers\userController.js
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 
 // login user
